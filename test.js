517f9ba20d46b4f38d3b65d28a6cf72b9d56db72
Tobias Herber <40146720150275@litec.ac.at>
2018-03-26 21:28:22 +0200

    add tests for computed values

--- test.js
+++ test.js
@@ -61,3 +61,62 @@ test('initial state', () => {
   expect(state1.state().other).toBe('initial');
   expect(state1.state()).toEqual({ foo: 'bar', bar: 'baz', init: true, other: 'initial' });
 });
+
+test('calculates computed state', () => {
+  const state1 = nation();
+
+  state1.setState({ foo: 'bar', bar: 'baz' });
+
+  expect(state1.state().foo).toBe('bar');
+  expect(state1.state().bar).toBe('baz');
+  expect(state1.state()).toEqual({ foo: 'bar', bar: 'baz' });
+
+  state1.computed(state => ({
+    fooBar: () => {
+      return `${state().foo} - ${state().bar}`;
+    }
+  }));
+
+  expect(state1.state().foo).toBe('bar');
+  expect(state1.state().bar).toBe('baz');
+  expect(state1.state().fooBar).toBe('bar - baz');
+  expect(state1.state()).toEqual({ foo: 'bar', bar: 'baz', fooBar: 'bar - baz' });
+
+  state1.setState({ foo: 'foo' });
+
+  expect(state1.state().foo).toBe('foo');
+  expect(state1.state().bar).toBe('baz');
+  expect(state1.state().fooBar).toBe('foo - baz');
+  expect(state1.state()).toEqual({ foo: 'foo', bar: 'baz', fooBar: 'foo - baz' });
+});
+
+test('state change events', done => {
+  const state1 = nation();
+  let run = 0;
+
+  state1.emitter.on('state-change', state => {
+    run++;
+
+    if (run == 2) done();
+  });
+
+  state1.setState({ foo: 'bar', bar: 'baz' });
+
+  expect(state1.state().foo).toBe('bar');
+  expect(state1.state().bar).toBe('baz');
+  expect(state1.state()).toEqual({ foo: 'bar', bar: 'baz' });
+
+  state1.setState({ bar: 'x' });
+
+  state1.action(state => ({
+    setFoo: val => {
+      state().foo = val;
+    }
+  }));
+
+  state1.actions().setFoo('y');
+
+  expect(state1.state().foo).toBe('y');
+  expect(state1.state().bar).toBe('x');
+  expect(state1.state()).toEqual({ foo: 'y', bar: 'x' });
+});
